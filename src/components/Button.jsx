e90f30b601caab56975d7778755ed9f033ddbc64
johnpark3002 <john.park2@mail.mcgill.ca>
2025-02-07 22:55:11 -0500

    Changed visibility of resume

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-import Resume from "../assets/resume/john-park-software-engineering-resume.pdf";
+import Resume from "../../public/resume/john-park-software-engineering-resume.pdf";
 import { DOWNLOAD_RESUME } from "../constants";
 import { useLanguage } from "../languageContext";
 
@@ -8,7 +8,10 @@ const Button = () => {
   return (
     <button className="cursor-pointer bg-gradient-to-b from-indigo-500 to-indigo-600 shadow-[0px_4px_32px_0_rgba(99,102,241,.70)] px-6 py-3 rounded-xl border-[1px] border-slate-500 text-white font-medium group">
       <div className="relative overflow-hidden">
-        <a href={Resume} download="john-park-software-engineering-resume.pdf">
+        <a
+          href="/resume/john-park-software-engineering-resume.pdf?url"
+          download="john-park-software-engineering-resume.pdf"
+        >
           <p className="group-hover:-translate-y-7 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]">
             {DOWNLOAD_RESUME[language]}
           </p>
